54d415fec904459b131386dd30ba957d98785268
Dario Cruz <rio2012@gmail.com>
2023-03-20 13:45:41 -0400

    Add js code for adding and removing active class. Add css props for displaying and hiding menu links.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const burgerMenu = document.querySelector('.hamburger-menu')
-const navLinkes = document.querySelector('ul')
+const burgerMenu = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+const navLink = document.querySelectorAll('.nav-link')
+
+navLink.forEach(n => n.addEventListener('click', () => {
+    burgerMenu.classList.remove('active')
+    navMenu.classList.remove('active')
+}))
 
